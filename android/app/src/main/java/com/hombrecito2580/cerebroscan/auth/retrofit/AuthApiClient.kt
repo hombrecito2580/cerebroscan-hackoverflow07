9b498e9a142820a0503115a47f8314cb55ecff86
hombrecito2580 <hombrecito2580@gmail.com>
2024-04-08 23:19:10 +0530

    Properties Set Android

--- android/app/src/main/java/com/hombrecito2580/cerebroscan/auth/retrofit/AuthApiClient.kt
+++ android/app/src/main/java/com/hombrecito2580/cerebroscan/auth/retrofit/AuthApiClient.kt
@@ -2,9 +2,16 @@ package com.hombrecito2580.cerebroscan.auth.retrofit
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.FileInputStream
+import java.util.Properties
 
 object AuthApiClient {
-    private const val BASE_URL = "http://192.168.105.36:8000/"
+    private val envProperties = Properties().apply {
+        val inputStream = FileInputStream("env.properties")
+        load(inputStream)
+    }
+
+    private val BASE_URL = envProperties.getProperty("BASE_URL")
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
